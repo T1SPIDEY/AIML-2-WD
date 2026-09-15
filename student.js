6314af0d838d87cbccbf28b80e96d7938a41a6d8
T1SPIDEY <workdevanshaga@gmail.com>
2026-09-15 10:56:33 +0530

    addeD

---
+++ student.js
@@ -0,0 +1,12 @@
+class student
+{
+    name;
+     roll_no;
+     constructor(name,roll_no)
+     {
+        name=name;
+        roll_no=roll_no;
+        console.log("Name: "+name);
+        console.log("Roll No: "+roll_no);   
+     }
+};
